6cac06d85ce6eed1ec94000bde54fc7a71fff527
Noor-A-Alam <noorsomrat2000@gmail.com>
2024-01-09 18:12:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-2xl bg-red-600'>Knowledge Caffee</h1>
+     
+    </>
+  )
+}
+
+export default App
